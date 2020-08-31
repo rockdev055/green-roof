4401db054faba08a1e3505d35a205d42a26408c0
rockdev55 <steve264225@gmail.com>
2020-08-31 01:42:13 -0700

    implemented connection with database

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ const db = require('./database');
 const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
-const fetch = require('node-fetch');
-const bcrypt = require('bcrypt');
+// const fetch = require('node-fetch');
 
 const app = express();
 
@@ -22,6 +21,60 @@ app.get('/api/health-check', (req, res, next) => {
 
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
+});
+
+// app.get('/api/users/', (req, res, next) => {
+//   const userInfo = `
+//     select *
+//     from "users"
+//     where "userId" = $1
+//   `;
+//   if (req.session.userId) {
+//     const params = [req.session.userId];
+//     db.query(userInfo, params)
+//       .then(userData => {
+//         res.status(200).json(userData.rows[0]);
+//       })
+//       .catch(err => console.error(err));
+//   } else {
+//     next(new ClientError('No user was logged in', 200));
+//   }
+// });
+
+app.get('/api/signers', (req, res, next) => {
+  const allSigners = `
+      select *
+      from "signers"
+      `;
+  db.query(allSigners)
+    .then(signersData => {
+      res.status(200).json(signersData.rows[0]);
+    })
+    .catch(err => console.error(err));
+});
+
+app.post('/api/signers', (req, res, next) => {
+  const parsedName = req.body.signerName;
+  const parsedEmail = req.body.signerEmail;
+
+  const text = `insert into "singers" ("signerName", "signerEmail")
+                values($1, $2)
+                returning *`;
+  const values = [parsedName, parsedEmail];
+
+  db.query(text, values)
+    .then(result => {
+      const user = result.rows[0];
+      req.session.userId = user.userId;
+      res.json(user);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occured.'
+      });
+      console.error(err);
+    });
 });
 
 app.use((err, req, res, next) => {
