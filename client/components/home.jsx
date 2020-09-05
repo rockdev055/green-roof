52a66551c2fdda5d94b7a96aa342c7b2a4eec43f
rockdev55 <steve264225@gmail.com>
2020-09-04 22:19:03 -0700

    created new styles for the home page

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -10,11 +10,21 @@ class Home extends React.Component {
             <span>Keep UCSB clean, make the roofs green</span>
           </div>
         </div>
+        <div className="text-center home-sign-petition">
+          <div className="row">
+            <div className="inner-card">
+              <h1>Sign the Petition</h1>
+            </div>
+            <div className="inner-card">
+              <div>Sign the Petition</div>
+            </div>
+          </div>
+        </div>
+        <h1 className="text-center home-header">
+          Our Goal
+        </h1>
         <div className="text-center home-container">
 
-          <h1 className="">
-          Our Goal
-          </h1>
           <main className="home-text">
             <p>
             UCSB Green Roofs is a campaign focused on making our campus more sustainable by incorporating green roofs on all applicable buildings.
