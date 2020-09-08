25070b1e515bb32042153afcf8f360e6884b8da3
rockdev55 <steve264225@gmail.com>
2020-09-07 22:27:07 -0700

    added about page info

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -6,7 +6,39 @@ class About extends React.Component {
       <div>
         <div className="home-banner-container">
         </div>
-        Dope
+        <div className="why-container">
+          <h1 className="why-title">Why Respond</h1>
+          <section className="why-info-container">
+            <div className="why-info">
+              <i className="fas fa-cloud-rain"></i>
+              <span className="why-text">Reduce water runoff to improve the health of the aquatic environment near campus.</span>
+            </div>
+            <div className="why-info">
+              <i className="fas fa-utensils"></i>
+              <span className="why-text">Lower food costs for dining halls to benefit the university and students.</span>
+            </div>
+            <div className="why-info">
+              <i className="fas fa-hamburger"></i>
+              <span className="why-text">Promote food security among college students. About 48% of UCSB students are food insecure.</span>
+            </div>
+            <div className="why-info">
+              <i className="far fa-smile-beam"></i>
+              <span className="why-text">Bring positive attention to UCSB and showcase the innovative spirit of our students and commitment to sustainability.</span>
+            </div>
+            <div className="why-info">
+              <i className="fas fa-book"></i>
+              <span className="why-text">Green Roofs can be used as interactive classrooms to enchance learning in subjects such as ecology and botany.</span>
+            </div>
+            <div className="why-info">
+              <i className="far fa-building"></i>
+              <span className="why-text">Reduces carbon emissions coming from the buildings and would coincide with UCSB&apos;s zero carbon emissions initiative.</span>
+            </div>
+            <div className="why-info">
+              <i className="fas fa-sun"></i>
+              <span className="why-text">Green Roofs help reduce the heat island effect and keep the campus cooler.</span>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
